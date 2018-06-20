1f37f05b1f7d51a7a731c0b7723be936bcdd5adf
tomoya.miwa <tomoya.miwa@dena.com>
2018-06-20 19:13:11 +0900

    Support simple transition

--- src/main/kotlin/Fsm.kt
+++ src/main/kotlin/Fsm.kt
@@ -5,14 +5,14 @@ interface BaseEvent
 // TODO: detect errors at compile time
 // TODO: reconsidering how to write edges
 
-class StateMachine<T : BaseState>(initial: T) {
+class StateMachine(initial: BaseState) {
 
     private val fsmContext = FsmContext(initial)
     val children: MutableList<State> = mutableListOf()
 
     fun dispatch(event: BaseEvent): BaseState {
         // TODO: return new state
-        return fsmContext.state
+        return fsmContext.dispatch(event, children)
     }
 
     override fun toString(): String {
@@ -38,22 +38,41 @@ class Edge(val event: BaseEvent, val next: BaseState, val action: () -> Unit) {
     }
 }
 
-class FsmContext<T : BaseState>(initial: T) {
+class FsmContext(initial: BaseState) {
 
-    var state: T = initial
+    var state: BaseState = initial
         private set
 
-    fun dispatch(event: BaseEvent): T {
+    fun dispatch(event: BaseEvent, children: List<State>): BaseState {
+        val prev = state
+        val next = findEdgeRecursive(children, event)?.next ?: prev
+        state = next
+
         return state
     }
+
+    // TODO: tailrec
+    private fun findEdgeRecursive(states: List<State>, event: BaseEvent): Edge? {
+        return states.findEdge(event) ?: kotlin.run {
+            var edge: Edge? = null
+            states.forEach {
+                edge = findEdgeRecursive(it.children, event)
+                if (edge != null) return@run edge
+            }
+            return@run edge
+        }
+    }
+
+    private fun List<State>.findEdge(event: BaseEvent): Edge? =
+            this.flatMap { it.edges }.firstOrNull { it.event == event }
 }
 
 fun stateMachine(
         initial: BaseState,
-        init: StateMachine<*>.() -> Unit
-): StateMachine<*> = StateMachine(initial = initial).apply(init)
+        init: StateMachine.() -> Unit
+): StateMachine = StateMachine(initial = initial).apply(init)
 
-fun StateMachine<*>.state(
+fun StateMachine.state(
         state: BaseState,
         entry: () -> Unit = {},
         exit: () -> Unit = {},
